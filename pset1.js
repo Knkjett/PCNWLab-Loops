fe054b0acb7e8294b3f038cfc44aa2da303f3ec4
Kevin Zheng <kevinzheng@pursuit.org>
2018-10-20 15:28:32 -0400

    completed pset1

--- pset1.js
+++ pset1.js
@@ -6,7 +6,15 @@
             find average of all numbers
     @example - average([1,2,3,4]); // 2.5
 */
-
+const average = arr => {
+    let avg = 0 
+    for(let i = 0; i < arr.length; i++){
+        avg += arr[i];
+    }
+    return avg/arr.length;
+}
+console.log("---average---")
+console.log("average of the numbers [1,2,3,4] is " , average([1,2,3,4]));
 /*
     @func squareEach
     @param {array} arr
@@ -14,7 +22,14 @@
     @desc - square each value in array
     @example - squareEach([1,2,3,4]); // [1,4,9,16]
 */
-
+const squareEach = arr => {
+    for(let i = 0; i < arr.length; i++){
+        arr[i] = (i+1)*(i+1);
+    }
+    return arr;
+}
+console.log("---square each---")
+console.log("[1,2,3,4] squared is " , squareEach([1,2,3,4]));
 /*
     @func averageSquare
     @param {array} arr
@@ -28,7 +43,16 @@
         // then find the averate of the array to the right
         averageSquare([1,2,3,4); // 7.5
 */
-
+const averageSquare = arr => {
+    let avg = 0;
+    for(let i = 0; i < arr.length; i++){
+        arr[i] = (i+1)*(i+1);
+        avg += arr[i];
+    }
+    return avg/arr.length;
+}
+console.log("---average square---");
+console.log("Average of Squared [1,2,3,4] is ", averageSquare([1,2,3,4]));
 /*
     @func negateArr
     @param {array} arr
@@ -37,7 +61,14 @@
     @example
         negateArr([1,2,3,4]); // [-1,-2,-3,-4]
 */
-
+const negateArr = arr => {
+    for(let i = 0; i < arr.length; i++){
+        arr[i] *= -1;
+    }
+    return arr;
+}
+console.log("---negate arr---");
+console.log("Negating [1,2,3,4] is ", negateArr([1,2,3,4]));
 /*
     @func reverseArr
     @param {array} arr
@@ -45,7 +76,15 @@
     @desc - reverse an array
     @example - reverseArr([1,2,3,4]); // [4,3,2,1]
 */
-
+const reverseArr = arr => {
+    let arr2 = [];
+    for(let i = 0; i < arr.length; i++){
+        arr2[arr.length-1-i] = arr[i];
+    }
+    return arr2;
+}
+console.log("---reverse arr---");
+console.log("Array [1,2,3,4] reversed is ", reverseArr([1,2,3,4]));
 /*
     @func negateBackwards
     @param {array} arr
@@ -54,6 +93,12 @@
     @example
         negateBackwards([1,2,3,4]); // [-4, -3, -2, -1]
 */
-
-
-
+const negateBackwards = arr => {
+    let arr2 = [];
+    for(let i = 0; i < arr.length; i++){
+        arr2[arr.length-1-i] = arr[i]*-1;
+    }
+    return arr2;
+}
+console.log("---negate backwards---");
+console.log("Array [1,2,3,4] reversed and backwards is ", negateBackwards([1,2,3,4]));
